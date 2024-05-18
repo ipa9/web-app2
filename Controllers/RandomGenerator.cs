6024ad32367cb8a2de73b81a5da736ac3e11a7ae
PC <PC@DESKTOP-7450GMH>
2024-05-18 18:01:32 +0400

    changed

---
+++ Controllers/RandomGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomGenerator : ControllerBase
+    {
+        private readonly RandomGeneratorService _randomGeneratorService;
+        private readonly RandomGeneratorService randomGeneratorService2;
+
+        public RandomGenerator(RandomGeneratorService randomGeneratorService, RandomGeneratorService randomGeneratorService2)
+        {
+            _randomGeneratorService = randomGeneratorService;
+            this.randomGeneratorService2 = randomGeneratorService2;
+        }
+        [HttpGet]
+        public string Get()
+        {
+            return _randomGeneratorService.RandomString + "     " + randomGeneratorService2.RandomString;
+        }
+    }
+}
---
+++ Services/RandomGeneratorService.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Services
+{
+    public class RandomGeneratorService
+    {
+        public string RandomString { get; set; }
+        public RandomGeneratorService()
+        {
+            var random = new Random();
+            RandomString = random.Next(1, 1000).ToString();
+        }
+    }
+}
